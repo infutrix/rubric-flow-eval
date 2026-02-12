b09f0a98f6cc08362d2daa62bce859d4502bc982
Sahil Kumar <sahilkumar142002@gmail.com>
2026-02-12 18:41:38 +0530

    Update Navigation component logo styling for improved appearance

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -41,7 +41,7 @@ const Navigation = () => {
               <img
                 src={logoGradesmith}
                 alt="Gradesmith"
-                className="h-10 md:h-12 w-auto transition-transform hover:scale-105"
+                className="h-10 md:h-12 w-auto transition-transform hover:scale-105 bg-black py-2 pb-3 px-4 rounded-2xl"
               />
             </a>
 
